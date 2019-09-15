23c0061b4ab74c7f47d73d10832e51b4b52c3906
Maganjot Singh <maganjotsingh@gmail.com>
2019-09-16 00:09:49 +0530

    Add files via upload

---
+++ random_password_generator.py
@@ -0,0 +1,24 @@
+
+
+#!/bin/python3
+import random
+
+char = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
+
+long = input("Total Number of passwords?")
+
+long = int(long)
+
+for p in range(long):
+ 
+    length = input("Password Length?")
+  
+    length = int(length)
+
+    password = ''
+  
+    for c in range(length):
+    
+       password += random.choice(char)
+  
+    print (password)
